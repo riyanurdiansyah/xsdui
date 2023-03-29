946e67e5ebc2818dd8eeebed94319cf2adef177b
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-03-29 10:53:06 +0700

    update scaffold

--- lib/parser/xsdui_scaffold/xsdui_scaffold.dart
+++ lib/parser/xsdui_scaffold/xsdui_scaffold.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:xsdui/parser/xsdui_appbar/xsdui_appbar.dart';
+import 'package:xsdui/xsdui.dart';
 
 class XSduiScaffold {
   static Widget fromJson(
@@ -8,5 +9,6 @@ class XSduiScaffold {
   }) =>
       Scaffold(
         appBar: XSduiAppbar.fromJson(context, json: json["appBar"]),
+        body: XSdui.fromJson(context, json: json["body"]),
       );
 }
