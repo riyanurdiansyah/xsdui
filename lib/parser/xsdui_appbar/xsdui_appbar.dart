1f074a8305e9a97c38a55bad289d41bf0fc8486a
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-12 13:43:03 +0700

    app bar to json

--- lib/parser/xsdui_appbar/xsdui_appbar.dart
+++ lib/parser/xsdui_appbar/xsdui_appbar.dart
@@ -6,22 +6,41 @@ class XSduiAppbar {
   static AppBar fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      AppBar(
-        centerTitle: json["centerTitle"],
-        automaticallyImplyLeading: json["automaticallyImplyLeading"] ?? true,
-        title: XSdui.fromJson(context, json: json["title"]),
-        backgroundColor: json["backgroundColor"] == null
-            ? null
-            : HexColor.fromHex(json["backgroundColor"]),
-        elevation: json["elevation"],
-        titleSpacing: json["titleSpacing"],
-        actions: List.generate(
-          json["actions"]?.length ?? 0,
-          (index) => XSdui.fromJson(context, json: json["actions"][index]),
-        ),
-        leading: json["leading"] == null
-            ? null
-            : XSdui.fromJson(context, json: json["leading"]),
-      );
+  }) {
+    return AppBar(
+      centerTitle: json["centerTitle"],
+      automaticallyImplyLeading: json["automaticallyImplyLeading"] ?? true,
+      title: XSdui.fromJson(context, json: json["title"]),
+      backgroundColor: json["backgroundColor"] == null
+          ? null
+          : HexColor.fromHex(json["backgroundColor"]),
+      elevation: json["elevation"],
+      titleSpacing: json["titleSpacing"],
+      actions: List.generate(
+        json["actions"]?.length ?? 0,
+        (index) => XSdui.fromJson(context, json: json["actions"][index]),
+      ),
+      leading: json["leading"] == null
+          ? null
+          : XSdui.fromJson(context, json: json["leading"]),
+    );
+  }
+
+  static Map<String, dynamic>? toJson({
+    required AppBar widget,
+  }) {
+    return {
+      "centerTitle": widget.centerTitle,
+      "automaticallyImplyLeading": widget.automaticallyImplyLeading,
+      "title": XSdui.toJson(widget: widget.title),
+      "backgroundColor": widget.backgroundColor == null
+          ? null
+          : HexColor.toHex(widget.backgroundColor!),
+      "elevation": widget.elevation,
+      "titleSpacing": widget.titleSpacing,
+      "actions": List.generate(widget.actions?.length ?? 0,
+          (index) => XSdui.toJson(widget: widget.actions![index])),
+      "leading": XSdui.toJson(widget: widget.leading),
+    };
+  }
 }
