eae24972c4ccbb0216f77707554fa31eacd96c7e
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-12 11:39:26 +0700

    color to json

--- lib/utils/xsdui_extension.dart
+++ lib/utils/xsdui_extension.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 extension HexColor on Color {
-  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
   static Color fromHex(String hexString) {
     final buffer = StringBuffer();
     if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
@@ -9,10 +8,7 @@ extension HexColor on Color {
     return Color(int.parse(buffer.toString(), radix: 16));
   }
 
-  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-      '${alpha.toRadixString(16).padLeft(2, '0')}'
-      '${red.toRadixString(16).padLeft(2, '0')}'
-      '${green.toRadixString(16).padLeft(2, '0')}'
-      '${blue.toRadixString(16).padLeft(2, '0')}';
+  static String toHex(Color color) {
+    return color.value.toRadixString(16).substring(2);
+  }
 }
