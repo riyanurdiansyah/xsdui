00920e85fe13b2f40b07e3cd766c96b38044b3da
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-13 14:20:22 +0700

    expanded to json

--- lib/parser/xsdui_expanded/xsdui_expanded.dart
+++ lib/parser/xsdui_expanded/xsdui_expanded.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:xsdui/xsdui.dart';
 
 class XSduiExpanded {
-  static Widget parse(
+  static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      Expanded(
-        flex: json["flex"],
-        child: XSdui.fromJson(
-          context,
-          json: json["child"],
-        ),
-      );
+  }) {
+    return Expanded(
+      flex: json["flex"],
+      child: XSdui.fromJson(
+        context,
+        json: json["child"],
+      ),
+    );
+  }
+
+  static Map<String, dynamic>? toJson(Expanded widget) {
+    return {
+      "flex": widget.flex,
+      "child": XSdui.toJson(widget.child),
+    };
+  }
 }
