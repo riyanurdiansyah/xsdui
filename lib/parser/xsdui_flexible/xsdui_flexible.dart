48b63cad91ef88a637b0d6945d6fc340fa51ff48
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-13 14:20:31 +0700

    flexible to json

--- lib/parser/xsdui_flexible/xsdui_flexible.dart
+++ lib/parser/xsdui_flexible/xsdui_flexible.dart
@@ -2,15 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:xsdui/xsdui.dart';
 
 class XSduiFlexible {
-  static Widget parse(
+  static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      Flexible(
-        flex: json["flex"],
-        child: XSdui.fromJson(
-          context,
-          json: json["child"],
-        ),
-      );
+  }) {
+    return Flexible(
+      flex: json["flex"],
+      child: XSdui.fromJson(
+        context,
+        json: json["child"],
+      ),
+    );
+  }
+
+  static Map<String, dynamic>? toJson(Flexible widget) {
+    return {
+      "flex": widget.flex,
+      "child": XSdui.toJson(widget.child),
+    };
+  }
 }
