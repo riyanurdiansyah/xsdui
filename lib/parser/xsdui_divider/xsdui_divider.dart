40d5e65e16d9c9425ba1e1974b21042e97808cc3
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-13 11:47:41 +0700

    divider to json

--- lib/parser/xsdui_divider/xsdui_divider.dart
+++ lib/parser/xsdui_divider/xsdui_divider.dart
@@ -5,12 +5,23 @@ class XSduiDivider {
   static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      Divider(
-        color: json["color"] == null ? null : HexColor.fromHex(json["color"]),
-        endIndent: json["endIndent"],
-        height: json["height"],
-        thickness: json["thickness"],
-        indent: json["indent"],
-      );
+  }) {
+    return Divider(
+      color: json["color"] == null ? null : HexColor.fromHex(json["color"]),
+      endIndent: json["endIndent"],
+      height: json["height"],
+      thickness: json["thickness"],
+      indent: json["indent"],
+    );
+  }
+
+  static Map<String, dynamic>? toJson(Divider widget) {
+    return {
+      "color": widget.color == null ? null : HexColor.toHex(widget.color!),
+      "endIndent": widget.endIndent,
+      "height": widget.height,
+      "thickness": widget.thickness,
+      "indent": widget.indent,
+    };
+  }
 }
