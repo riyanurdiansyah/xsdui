aa23be04638dc543c0dbb2368463b5059ca560f2
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-13 11:47:20 +0700

    elevated button to json

--- lib/parser/xsdui_elevated_button/xsdui_elevated_button.dart
+++ lib/parser/xsdui_elevated_button/xsdui_elevated_button.dart
@@ -7,28 +7,36 @@ class XSduiElevatedButton {
   static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      ElevatedButton(
-        onPressed: () {
-          if (json["onPressed"]["type"] == "http") {
-            XSduiNetwork.getRequest(url: json["onPressed"]["url"]);
-          }
-        },
-        style: ButtonStyle(
-          backgroundColor: json["color"] == null
-              ? null
-              : MaterialStateProperty.all(HexColor.fromHex(json["color"])),
-          elevation: json["elevation"] == null
-              ? null
-              : MaterialStateProperty.all(json["elevation"]),
-          padding: json["padding"] == null
-              ? null
-              : MaterialStateProperty.all(
-                  XSduiEdgeInsetPadding.fromMap(json["padding"]),
-                ),
-        ),
-        child: json["child"] == null
-            ? const SizedBox()
-            : XSdui.fromJson(context, json: json["child"]),
-      );
+  }) {
+    return ElevatedButton(
+      onPressed: () {
+        if (json["onPressed"]["type"] == "http") {
+          XSduiNetwork.getRequest(url: json["onPressed"]["url"]);
+        }
+      },
+      style: ButtonStyle(
+        backgroundColor: json["color"] == null
+            ? null
+            : MaterialStateProperty.all(HexColor.fromHex(json["color"])),
+        elevation: json["elevation"] == null
+            ? null
+            : MaterialStateProperty.all(json["elevation"]),
+        padding: json["padding"] == null
+            ? null
+            : MaterialStateProperty.all(
+                XSduiEdgeInsetPadding.fromMap(json["padding"]),
+              ),
+      ),
+      child: json["child"] == null
+          ? const SizedBox()
+          : XSdui.fromJson(context, json: json["child"]),
+    );
+  }
+
+  static Map<String, dynamic>? toJson(ElevatedButton widget) {
+    return {
+      "style": widget.style.toString(),
+      "child": XSdui.toJson(widget.child),
+    };
+  }
 }
