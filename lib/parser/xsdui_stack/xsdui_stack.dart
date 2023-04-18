08ab9c8572c552c982ef1106b1c1bd2afd251ab9
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-18 09:27:47 +0700

    stack to json

--- lib/parser/xsdui_stack/xsdui_stack.dart
+++ lib/parser/xsdui_stack/xsdui_stack.dart
@@ -8,17 +8,34 @@ class XSduiStack {
   static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      Stack(
-        alignment: XSduiAlignment.fromString(json["alignment"]),
-        fit: XSduiStackFit.fromString(json["stackFit"]),
-        textDirection: json["textDirection"] == null
-            ? null
-            : XSduiTextDirection.fromString(json["textDirection"]),
-        clipBehavior: XSduiClipBehavior.fromString(json["clipBehavior"]),
-        children: List.generate(
-          json["children"] == null ? 0 : json["children"].length,
-          (index) => XSdui.fromJson(context, json: json["children"][index]),
-        ),
-      );
+  }) {
+    return Stack(
+      alignment: XSduiAlignment.fromString(json["alignment"]),
+      fit: XSduiStackFit.fromString(json["stackFit"]),
+      textDirection: json["textDirection"] == null
+          ? null
+          : XSduiTextDirection.fromString(json["textDirection"]),
+      clipBehavior: XSduiClipBehavior.fromString(json["clipBehavior"]),
+      children: List.generate(
+        json["children"] == null ? 0 : json["children"].length,
+        (index) => XSdui.fromJson(context, json: json["children"][index]),
+      ),
+    );
+  }
+
+  static Map<String, dynamic>? toJson(Stack widget) {
+    return {
+      "type": "Stack",
+      "alignment": widget.alignment.toString() == "null"
+          ? null
+          : widget.alignment.toString().replaceAll("Alignment.", ""),
+      "fit": widget.fit.toString() == "null"
+          ? null
+          : widget.fit.toString().replaceAll("StackFit.", ""),
+      "textDirection": XSduiTextDirection.convertToString(widget.textDirection),
+      "clipBehavior": XSduiClipBehavior.convertToString(widget.clipBehavior),
+      "children": List.generate(widget.children.length,
+          (index) => XSdui.toJson(widget.children[index])),
+    };
+  }
 }
--- lib/parser/xsdui_stack/xsdui_stack_extension.dart
+++ lib/parser/xsdui_stack/xsdui_stack_extension.dart
@@ -45,6 +45,48 @@ extension XSduiAlignment on AlignmentGeometry {
         return AlignmentDirectional.topStart;
     }
   }
+
+  static String? convertToString(AlignmentGeometry? align) {
+    if (align == Alignment.center) {
+      return "center";
+    }
+    if (align == Alignment.topCenter) {
+      return "topCenter";
+    }
+
+    if (align == Alignment.topLeft) {
+      return "topLeft";
+    }
+
+    if (align == Alignment.topRight) {
+      return "topRight";
+    }
+
+    if (align == Alignment.bottomCenter) {
+      return "bottomCenter";
+    }
+
+    if (align == Alignment.bottomLeft) {
+      return "bottomLeft";
+    }
+
+    if (align == Alignment.bottomRight) {
+      return "bottomRight";
+    }
+
+    if (align == Alignment.center) {
+      return "center";
+    }
+
+    if (align == Alignment.centerLeft) {
+      return "centerLeft";
+    }
+
+    if (align == Alignment.centerRight) {
+      return "centerRight";
+    }
+    return null;
+  }
 }
 
 extension XSduiStackFit on StackFit {
@@ -77,6 +119,19 @@ extension XSduiClipBehavior on Clip {
         return Clip.none;
       default:
         return Clip.hardEdge;
+    }
+  }
+
+  static String? convertToString(Clip clip) {
+    switch (clip) {
+      case Clip.antiAlias:
+        return "antiAlias";
+      case Clip.antiAliasWithSaveLayer:
+        return "antiAliasWithSaveLayer";
+      case Clip.none:
+        return "none";
+      default:
+        return null;
     }
   }
 }
