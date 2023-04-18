955f0dc7e286bb8b9b842dda66a097dd038ed188
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-18 09:26:25 +0700

    inkwell to json

--- lib/parser/xsdui_inkwell/xsdui_inkwell.dart
+++ lib/parser/xsdui_inkwell/xsdui_inkwell.dart
@@ -7,14 +7,29 @@ class XSduiInkwell {
   static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      InkWell(
-        onTap: () {},
-        borderRadius: json["borderRadius"] == null
-            ? null
-            : XSduiBorderRadius.fromMap(json["borderRadius"]),
-        child: json["child"] == null
-            ? const SizedBox()
-            : XSdui.fromJson(context, json: json["child"]),
-      );
+  }) {
+    return InkWell(
+      onTap: () {},
+      borderRadius: json["borderRadius"] == null
+          ? null
+          : XSduiBorderRadius.fromMap(json["borderRadius"]),
+      child: json["child"] == null
+          ? const SizedBox()
+          : XSdui.fromJson(context, json: json["child"]),
+    );
+  }
+
+  static Map<String, dynamic>? toJson(InkWell widget) {
+    return {
+      "type": "InkWell",
+      "borderRadius": {
+        "borderRadiusType": "ltrb",
+        "bottomLeft": (widget.borderRadius as BorderRadius).bottomLeft.x,
+        "bottomRight": (widget.borderRadius as BorderRadius).bottomRight.x,
+        "topLeft": (widget.borderRadius as BorderRadius).topLeft.x,
+        "topRight": (widget.borderRadius as BorderRadius).topRight.x,
+      },
+      "child": XSdui.toJson(widget.child),
+    };
+  }
 }
