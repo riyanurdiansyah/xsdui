e8ee054952ee3c93a1b7b5d097e4eb722ba77247
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-18 09:26:36 +0700

    list view to json

--- lib/parser/xsdui_list_view/xsdui_list_view_builder.dart
+++ lib/parser/xsdui_list_view/xsdui_list_view_builder.dart
@@ -10,36 +10,86 @@ class XSduiListViewBuilder {
   static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      ListView.builder(
-        addAutomaticKeepAlives: json["addAutomaticKeepAlives"] ?? true,
-        addRepaintBoundaries: json["addRepaintBoundaries"] ?? true,
-        addSemanticIndexes: json["addSemanticIndexes"] ?? true,
-        cacheExtent: json["cacheExtent"],
-        clipBehavior: XSduiClipBehavior.fromString(json["clipBehavior"]),
-        dragStartBehavior:
-            XSduiDragStartBehavior.fromString(json["dragStartBehavior"]),
-        itemExtent: json["itemExtent"],
-        keyboardDismissBehavior:
-            XSduiListViewKeyboardDissmissBehavior.fromString(
-                json["keyboardDismissBehavior"]),
-        padding: json["padding"] == null
-            ? null
-            : XSduiEdgeInsetPadding.fromMap(json["padding"]),
-        primary: json["primary"],
-        prototypeItem: json["prototypeItem"] == null
-            ? null
-            : XSdui.fromJson(context, json: json["prototypeItem"]),
-        restorationId: json["restorationId"],
-        reverse: json["reverse"] ?? false,
-        semanticChildCount: json["semanticChildCount"],
-        shrinkWrap: json["shrinkWrap"] ?? false,
-        scrollDirection: XSduiAxis.fromString(json["scrollDirection"]),
-        physics: json["physics"] == null
-            ? null
-            : XSduiScrollPhysics.fromString(json["physics"]),
-        itemCount: json["children"]?.length ?? 0,
-        itemBuilder: (context, index) =>
-            XSdui.fromJson(context, json: json["children"][index]),
-      );
+  }) {
+    return ListView.builder(
+      addAutomaticKeepAlives: json["addAutomaticKeepAlives"] ?? true,
+      addRepaintBoundaries: json["addRepaintBoundaries"] ?? true,
+      addSemanticIndexes: json["addSemanticIndexes"] ?? true,
+      cacheExtent: json["cacheExtent"],
+      clipBehavior: XSduiClipBehavior.fromString(json["clipBehavior"]),
+      dragStartBehavior:
+          XSduiDragStartBehavior.fromString(json["dragStartBehavior"]),
+      itemExtent: json["itemExtent"],
+      keyboardDismissBehavior: XSduiListViewKeyboardDissmissBehavior.fromString(
+          json["keyboardDismissBehavior"]),
+      padding: json["padding"] == null
+          ? null
+          : XSduiEdgeInsetPadding.fromMap(json["padding"]),
+      primary: json["primary"],
+      prototypeItem: json["prototypeItem"] == null
+          ? null
+          : XSdui.fromJson(context, json: json["prototypeItem"]),
+      restorationId: json["restorationId"],
+      reverse: json["reverse"] ?? false,
+      semanticChildCount: json["semanticChildCount"],
+      shrinkWrap: json["shrinkWrap"] ?? false,
+      scrollDirection: XSduiAxis.fromString(json["scrollDirection"]),
+      physics: json["physics"] == null
+          ? null
+          : XSduiScrollPhysics.fromString(json["physics"]),
+      itemCount: json["children"]?.length ?? 0,
+      itemBuilder: (context, index) =>
+          XSdui.fromJson(context, json: json["children"][index]),
+    );
+  }
+
+  static Map<String, dynamic> toJson(ListView widget) {
+    return {
+      "type": "ListViewBuilder",
+      "addAutomaticKeepAlives":
+          (widget.childrenDelegate as SliverChildBuilderDelegate)
+              .addAutomaticKeepAlives,
+      "addRepaintBoundaries":
+          (widget.childrenDelegate as SliverChildBuilderDelegate)
+              .addRepaintBoundaries,
+      "addSemanticIndexes":
+          (widget.childrenDelegate as SliverChildBuilderDelegate)
+              .addSemanticIndexes,
+      "cacheExtent": widget.cacheExtent,
+      "clipBehavior": widget.clipBehavior.toString().replaceAll("Clip.", ""),
+      "dragStartBehavior": widget.dragStartBehavior
+          .toString()
+          .replaceAll("DragStartBehavior.", ""),
+      "itemExtent": widget.itemExtent,
+      "keyboardDismissBehavior": widget.keyboardDismissBehavior
+          .toString()
+          .replaceAll("ScrollViewKeyboardDismissBehavior.", ""),
+      "padding": widget.padding == null
+          ? null
+          : {
+              "paddingType": "ltrb",
+              "left": (widget.padding as EdgeInsets).left,
+              "top": (widget.padding as EdgeInsets).top,
+              "right": (widget.padding as EdgeInsets).right,
+              "bottom": (widget.padding as EdgeInsets).bottom,
+            },
+      "primary": widget.primary,
+      "prototypeItem": XSdui.toJson(widget.prototypeItem),
+      "restorationId": widget.restorationId,
+      "reverse": widget.reverse,
+      "semanticChildCount": widget.semanticChildCount,
+      "shrinkWrap": widget.shrinkWrap,
+      "scrollDirection": widget.physics is ClampingScrollPhysics
+          ? "clamping"
+          : widget.physics is BouncingScrollPhysics
+              ? "bouncing"
+              : widget.physics is FixedExtentScrollPhysics
+                  ? "fixed"
+                  : widget.physics is NeverScrollableScrollPhysics
+                      ? "never"
+                      : widget.physics is PageScrollPhysics
+                          ? "page"
+                          : null,
+    };
+  }
 }
