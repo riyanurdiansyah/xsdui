fa665c53ae9aa18a098643542016191b5cf53c25
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-04-18 09:27:04 +0700

    padding to json

--- lib/parser/xsdui_padding/xsdui_padding.dart
+++ lib/parser/xsdui_padding/xsdui_padding.dart
@@ -7,11 +7,26 @@ class XSduiPadding {
   static Widget fromJson(
     BuildContext context, {
     required Map<String, dynamic> json,
-  }) =>
-      Padding(
-        padding: XSduiEdgeInsetPadding.fromMap(json["padding"]),
-        child: json["child"] == null
-            ? const SizedBox()
-            : XSdui.fromJson(context, json: json["child"]),
-      );
+  }) {
+    return Padding(
+      padding: XSduiEdgeInsetPadding.fromMap(json["padding"]),
+      child: json["child"] == null
+          ? const SizedBox()
+          : XSdui.fromJson(context, json: json["child"]),
+    );
+  }
+
+  static Map<String, dynamic>? toJson(Padding widget) {
+    return {
+      "type": "Padding",
+      "padding": {
+        "paddingType": "ltrb",
+        "left": (widget.padding as EdgeInsets).left,
+        "top": (widget.padding as EdgeInsets).top,
+        "right": (widget.padding as EdgeInsets).right,
+        "bottom": (widget.padding as EdgeInsets).bottom,
+      },
+      "child": XSdui.toJson(widget.child),
+    };
+  }
 }
