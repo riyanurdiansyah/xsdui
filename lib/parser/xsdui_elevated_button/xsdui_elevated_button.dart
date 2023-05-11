8d61cc100632fea793e01753369eeb73f7f8eae6
riyanurdiansyah <riyanurdiansyah@gmail.com>
2023-05-11 17:51:51 +0700

    added direct link url

--- lib/parser/xsdui_elevated_button/xsdui_elevated_button.dart
+++ lib/parser/xsdui_elevated_button/xsdui_elevated_button.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:url_launcher/url_launcher.dart";
 import "package:xsdui/parser/xsdui_container/xsdui_container_extension.dart";
 import "package:xsdui/utils/xsdui_extension.dart";
 import "package:xsdui/xsdui.dart";
@@ -9,8 +10,14 @@ class XSduiElevatedButton {
     required Map<String, dynamic> json,
     required Map<String, Function> functionMap,
   }) {
-    void handleAction() {
+    void handleAction() async {
       switch (json['onPressed']['type']) {
+        case 'link':
+          final checkUrl = await canLaunchUrl(json["onPressed"]["link"]);
+          if (checkUrl) {
+            launchUrl(json["onPressed"]["link"]);
+          }
+          break;
         case 'http':
           XSduiNetwork.getRequest(url: json["onPressed"]["url"]);
           break;
@@ -36,20 +43,43 @@ class XSduiElevatedButton {
 
     return ElevatedButton(
       onPressed: handleAction,
-      style: ButtonStyle(
-        backgroundColor: json["backgroundColor"] == null
-            ? null
-            : MaterialStateProperty.all(
-                HexColor.fromHex(json["backgroundColor"])),
-        elevation: json["elevation"] == null
-            ? null
-            : MaterialStateProperty.all(json["elevation"]),
+      style: ElevatedButton.styleFrom(
         padding: json["padding"] == null
             ? null
-            : MaterialStateProperty.all(
-                XSduiEdgeInsetPadding.fromMap(json["padding"]),
+            : XSduiEdgeInsetPadding.fromMap(json["padding"]),
+        backgroundColor:
+            json["color"] == null ? null : HexColor.fromHex(json["color"]),
+        shape: json["shape"] == null
+            ? null
+            : RoundedRectangleBorder(
+                borderRadius: json["shape"]["borderRadius"] == null
+                    ? BorderRadius.zero
+                    : XSduiBorderRadius.fromMap(json["shape"]["borderRadius"]),
+                side: json["shape"]["borderSide"] == null
+                    ? BorderSide.none
+                    : BorderSide(
+                        color: json["shape"]["borderSide"]["color"] == null
+                            ? const Color(0xFF000000)
+                            : HexColor.fromHex(
+                                json["shape"]["borderSide"]["color"],
+                              ),
+                      ),
               ),
       ),
+      // style: ButtonStyle(
+      //   backgroundColor: json["backgroundColor"] == null
+      //       ? null
+      //       : MaterialStateProperty.all(
+      //           HexColor.fromHex(json["backgroundColor"])),
+      //   elevation: json["elevation"] == null
+      //       ? null
+      //       : MaterialStateProperty.all(json["elevation"]),
+      //   padding: json["padding"] == null
+      //       ? null
+      //       : MaterialStateProperty.all(
+      //           XSduiEdgeInsetPadding.fromMap(json["padding"]),
+      //         ),
+      // ),
       child: json["child"] == null
           ? const SizedBox()
           : XSdui.fromJson(context, json: json["child"]),
